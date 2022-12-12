e57f400e0a6229925755f9d82a5407d038df443a
caioorleans <caioorleansdm@hotmail.com>
2022-12-12 15:46:44 -0300

    Componentes CurrentForecast e customTable criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CurrentWeatherComponent } from './components/current-weather/current-weather.component';
+import { CustomTableComponent } from './components/custom-table/custom-table.component';
+import { TableColumnComponent } from './components/custom-table/table-column/table-column.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentWeatherComponent,
+    CustomTableComponent,
+    TableColumnComponent,
   ],
   imports: [
     BrowserModule,
