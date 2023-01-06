24721237ce9421a925b180b177909ef944dd1019
caioorleans <caioorleansdm@hotmail.com>
2023-01-06 14:55:46 -0300

    Iniciada a configuração de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForecastComponent } from './pages/forecast/forecast.component';
+import { MessageComponent } from './pages/message/message.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'forecast', component: ForecastComponent},
+  {path:'message', component: MessageComponent},
+  {path:'', redirectTo:'/message', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
